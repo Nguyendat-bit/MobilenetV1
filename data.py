8b109c04ff523bd03c5834495726d2aaa3227261
superstarlol999 <34463182+superstarlol999@users.noreply.github.com>
2021-10-23 01:04:11 +0700

    fix_data

--- data.py
+++ data.py
@@ -21,7 +21,7 @@ class DataLoader():
                 rescale= 1. /255,
                 rotation_range= 20, 
                 width_shift_range= 0.2,
-
+                height_shift_range= 0.2, 
                 shear_range= 0.2 , 
                 zoom_range= 0.2,
                 horizontal_flip= True,
